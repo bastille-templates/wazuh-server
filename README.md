e9a4da6a31f15f6fe4730aafe429b640ed75b574
Asep Ready <asepdevops@gmail.com>
2025-05-20 14:30:01 +0700

    wazuh

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ The Wazuh server analyzes the data received from the Wazuh agents, triggering al
 
 ## Now apply template to container
 ```sh
-pkg info -D -x wazuh-server | less
+bastille pkg wazuh-server info -D -x wazuh-server | less
 ```
-Edii file /usr/local/bastille/jails/wazuh/fstab
+Edii file /usr/local/bastille/jails/wazuh-server/fstab
 ```sh
 fdesc	/dev/fd		fdescfs		rw	0	0
 proc	/proc		procfs		rw	0	0
--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 bastille create wazuh-server 14.2-RELEASE 10.0.0.20
+bastille cmd wazuh-server echo "10.0.0.20 server.wazuh" > /etc/hosts
 bastille cmd wazuh-server sed -e "s|quarterly|latest|g" -i.bak /etc/pkg/FreeBSD.conf
 bastille cmd wazuh-server sed -e "s|#PermitRootLogin no|PermitRootLogin yes|g" -i.bak /etc/ssh/sshd_config
 bastille cmd wazuh-server sed -e "s|#PasswordAuthentication no|PasswordAuthentication yes|g" -i.bak /etc/ssh/sshd_config
 bastille cmd wazuh-server sed -e "s|#PermitEmptyPasswords no|PermitEmptyPasswords yes|g" -i.bak /etc/ssh/sshd_config
 bastille cmd wazuh-server service sshd enable
 bastille cmd wazuh-server service sshd start
-bastille cmd wazuh-server passwd -p 'sshroot'
+bastille cmd wazuh-server passwd
 #cp config/* /usr/local/bastille/jails/wazuh-server/
 bastille stop wazuh-server; bastille start wazuh-server
 bastille pkg wazuh-server update -y; bastille pkg wazuh-server update
